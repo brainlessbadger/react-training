f5c01e2d8764ace1d0507b7400b2413cb59c11b9
2bad <kamil.mosior@onet.pl>
2020-11-04 21:45:25 +0100

    ...

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import './styles.css';
 
-render(
-    <div className='main'>
-        <h1 className='greeting'>
-            Hello, World!
-        </h1>
-    </div>,
-    document.querySelector('#root')
-)
+const helloWorld = <div className='main'>
+    <h1 className='greeting'>
+        Hello, World!
+    </h1>
+</div>
+
+render(helloWorld, document.querySelector('#root'));
