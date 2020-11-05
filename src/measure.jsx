f68e49d196950eed4c70c85eb9a581c23bbff2cb
2bad <kamil.mosior@onet.pl>
2020-11-05 22:46:14 +0100

    ...

--- src/measure.jsx
+++ src/measure.jsx
@@ -22,7 +22,7 @@ export const Measure = () => {
             Width: {width}px
         </span>
         <span className='center'>
-            Heigth: {height}px
+            Height: {height}px
         </span>
     </div>
 }
