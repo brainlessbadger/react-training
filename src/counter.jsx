d3c4e0122f36e9501e9ef54d5f2821869a589420
2bad <kamil.mosior@onet.pl>
2020-11-11 21:30:15 +0100

    ....

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react"
 
-export const Counter = () => {
-    const [count, setCount] = useState(0);
-    const increment = () => setCount(count => ++count);
+export const Counter = ({count, onChange}) => {
+    const clickHandler = () => {
+        onChange(count + 1);
+    }
 
     return <button 
         type="button" 
         className="button" 
-        onClick={increment}
+        onClick={clickHandler}
     >
         Clicked {count} times!
     </button>
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import './styles.css';
 import { Counter } from './counter';
 
 const Main = () => {
+    const [count1, setCount1] = useState(0);
+    const [count2, setCount2] = useState(0);
+    const [count3, setCount3] = useState(0);
+
+    const reset = () => {
+        setCount1(0);
+        setCount2(0);
+        setCount3(0);
+    }
+
     return <div>
-        <Counter />
-        <Counter />
-        <Counter />
+        <Counter count={count1} onChange={setCount1} />
+        <Counter count={count2} onChange={setCount2}  />
+        <Counter count={count3} onChange={setCount3}  />
+        <button 
+            type="button" 
+            className="button" 
+            onClick={reset}
+        >
+            RESET
+        </button>
     </div>
 }
 
